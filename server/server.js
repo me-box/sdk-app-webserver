1bea9b04c81b9cdbd6fa5280fb0d70adb9f9a681
tlodge <tlodge@gmail.com>
2017-03-14 20:26:35 +0000

    created https webserver

--- server/server.js
+++ server/server.js
@@ -1,22 +1,36 @@
-import http  from 'http';
+import https from 'https';
 import express from 'express';
 import bodyparser from 'body-parser';
 import websocketinit from './comms/websocket';
 import ipcinit from './comms/ipc';
 
 const app = express();
+
+var HTTPS_SERVER_CERT = process.env.HTTPS_SERVER_CERT || '';
+var HTTPS_SERVER_PRIVATE_KEY = process.env.HTTPS_SERVER_PRIVATE_KEY || '';
+
+var credentials = {
+	key:  HTTPS_SERVER_PRIVATE_KEY,
+	cert: HTTPS_SERVER_CERT,
+};
+
 app.use('/', express.static("static"));
 app.set('view engine', 'html');
 app.engine('html', require('ejs').renderFile);
 
-const server = http.createServer(app);
+console.log("STARTING HTTPS SERVER!!");
+console.log(credentials);
+
+const server = https.createServer(credentials, app);
 let PORT = 8080
 
 if (process.argv.length > 2){
    PORT = parseInt(process.argv[2]);
 }
-
+console.log("initing websockets");
 websocketinit(['databox'],server);
+
+console.log("initing ipc");
 ipcinit();
 
 app.get('/ui', function(req,res){
@@ -25,6 +39,7 @@ app.get('/ui', function(req,res){
 });
 
 app.get('/', function(req,res){
+  console.log("seen a get /");
   res.render('index');
 });
 
@@ -32,7 +47,8 @@ app.use('/comms', require('./routes/comms'));
 
 //redirect any failed routes to root
 app.use(function(req,res){
-   res.redirect("/");
+  	console.log("failed route - redirecting to /")
+   	res.redirect("/");
 });
 
 server.listen(PORT);
