b6c40d7f0e65eae96cbbde8a2d669761816fa9bb
tlodge <tlodge@gmail.com>
2017-06-01 14:35:03 +0100

    removed dev tools from production release

--- client/js/store/configureStore.prod.js
+++ client/js/store/configureStore.prod.js
@@ -1,20 +1,8 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
-import DevTools from '../containers/DevTools';
-import { persistState } from 'redux-devtools';
 
-//const enhancer = applyMiddleware(thunk);
-
-const enhancer = compose(
-  applyMiddleware(thunk),
-  DevTools.instrument(),
-  persistState(
-    window.location.href.match(
-      /[?&]debug_session=([^&#]+)\b/
-    )
-  )
-);
+const enhancer = applyMiddleware(thunk);
 
 export default function configureStore(initialState) {
   return createStore(rootReducer, initialState, enhancer);
