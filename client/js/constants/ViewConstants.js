02d2884961604bcdbdede07668e4acf5a25c37f6
tlodge <tlodge@gmail.com>
2017-06-06 15:40:07 +0100

    added in missing view constants

--- client/js/constants/ViewConstants.js
+++ client/js/constants/ViewConstants.js
@@ -1,1 +1,7 @@
- export const APP_TITLEBAR_HEIGHT = 0;
+export const HEADER_TOOLBAR_HEIGHT = 0;//60;
+export const FOOTER_TOOLBAR_HEIGHT = 0;//60;
+export const APP_TITLEBAR_HEIGHT = 0;
+export const TREEPADDING = 150;
+export const TREEMARGIN = 30;
+export const NODEWIDTH  = 40;
+export const NODEHEIGHT = 40;
